5370c850a79800d5450aeb59dd043e66b44f31b2
ShionGT <shion.toda@gmail.com>
2026-01-16 15:39:30 +0900

    setup

--- js/app.js
+++ js/app.js
@@ -1,19 +1,7 @@
-class Table {
-    constructor() {
-        this.player = new Player();
-        this.dealer = new Dealer();
-        this.deck = new Deck();
 
-        function draw() {
-            canv = document.getElementById("table-canvas");
-            ctx = canv.getContext("2d");
+setInterval(() => {
+            const canv = document.getElementById("table-canvas");
+            const ctx = canv.getContext("2d");
             ctx.fillStyle = "darkgreen";
             ctx.fillRect(0, 0, canv.width, canv.height);
-        }
-
-        draw();
-    }
-
-
-
-}
+}, 1000 / 30);
